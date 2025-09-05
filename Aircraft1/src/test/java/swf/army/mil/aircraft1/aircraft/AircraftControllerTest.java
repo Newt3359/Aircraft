368968c018c3173918503e89001d53a182ec75da
Dennis Gingrich <dennis.j.gingrich.mil@swf.army.mil>
2025-09-05 10:08:31 -0500

    added aircraft controller

---
+++ Aircraft1/src/test/java/swf/army/mil/aircraft1/aircraft/AircraftControllerTest.java
@@ -0,0 +1,11 @@
+package swf.army.mil.aircraft1.aircraft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AircraftControllerTest {
+
+    @Test
+
+}
