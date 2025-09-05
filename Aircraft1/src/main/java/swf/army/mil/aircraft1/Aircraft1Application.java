547a43de3bb48a0d376d90d82113a8afe3ee8abb
Dennis Gingrich <dennis.j.gingrich.mil@swf.army.mil>
2025-09-05 09:52:34 -0500

    changed yaml file

--- Aircraft1/src/main/java/swf/army/mil/aircraft1/Aircraft1Application.java
+++ Aircraft1/src/main/java/swf/army/mil/aircraft1/Aircraft1Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Aircraft1Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Aircraft1Application.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(Aircraft1Application.class, args);
+	}
 
 }
--- Aircraft1/src/test/java/swf/army/mil/aircraft1/Aircraft1ApplicationTests.java
+++ Aircraft1/src/test/java/swf/army/mil/aircraft1/Aircraft1ApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class Aircraft1ApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
