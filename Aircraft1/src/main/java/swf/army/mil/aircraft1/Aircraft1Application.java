1644a88d084c6c0d58b0a9824426b3c0f7d014b2
Dennis Gingrich <dennis.j.gingrich.mil@swf.army.mil>
2025-09-05 09:47:51 -0500

    added spring boot

---
+++ Aircraft1/src/main/java/swf/army/mil/aircraft1/Aircraft1Application.java
@@ -0,0 +1,13 @@
+package swf.army.mil.aircraft1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aircraft1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aircraft1Application.class, args);
+    }
+
+}
---
+++ Aircraft1/src/test/java/swf/army/mil/aircraft1/Aircraft1ApplicationTests.java
@@ -0,0 +1,13 @@
+package swf.army.mil.aircraft1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aircraft1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
